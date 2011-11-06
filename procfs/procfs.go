067aa9ee630f8a147f5dbc722d62ad408aedb480
Scott Lawrence <bytbox@gmail.com>
2011-11-06 12:38:23 -0500

    Filling out the ProcFS data structure

--- procfs/procfs.go
+++ procfs/procfs.go
@@ -1,10 +1,30 @@
 package procfs
 
 type ProcFS struct {
-
+	Processes map[int]Process
+	Self      Process
 }
 
 type Process struct {
-
+	Auxv    []byte
+	Cmdline []string
+	Cwd     string
+	Environ map[string]string
+	Exe     string
+	Fds     map[int]Fd
+	Root    string
+	Status  map[string]string
+	Threads map[int]Thread
 }
 
+// TODO limits, maps, mem, mountinfo, mounts, mountstats, ns, smaps, stat
+
+type Fd struct {
+	Path  string
+	Pos   int
+	Flags int
+}
+
+type Thread struct {
+	// TODO
+}

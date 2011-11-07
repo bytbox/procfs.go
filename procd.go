ac43a4813baf1d6b0eaf112cf622785c8c69c630
Scott Lawrence <bytbox@gmail.com>
2011-11-07 12:07:19 -0500

    Performing periodic updates

--- procd.go
+++ procd.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"github.com/bytbox/procfs.go/procfs"
-	"json"
+	"flag"
 	"log"
+	"time"
+	"sync"
+
+	"github.com/bytbox/procfs.go/procfs"
 )
 
+var pfs *procfs.ProcFS
+var pfsMutex sync.Mutex
+var interval = flag.Int64("interval", 10, "update interval")
+
 func main() {
+	flag.Parse()
+
+	go updater()
+
+/*
 	var pfs procfs.ProcFS
 	pfs.Fill()
 	str, err := json.MarshalIndent(pfs, "", "  ")
@@ -14,4 +26,24 @@ func main() {
 		log.Fatal("ERR: ", err)
 	}
 	println(string(str))
+*/
+
+	<-make(chan int)
+}
+
+// Maintain an updated version of ProcFS
+func updater() {
+	ticker := time.Tick(1e9 * *interval)
+	for true {
+		log.Print("Updating...")
+		sn := time.Nanoseconds()
+		var pfs2 procfs.ProcFS
+		pfs2.Fill()
+		pfsMutex.Lock()
+		pfs = &pfs2
+		pfsMutex.Unlock()
+		en := time.Nanoseconds()
+		log.Print("Done in ", (en-sn)/1000, "μs")
+		<-ticker
+	}
 }

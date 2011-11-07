edfd6983eb8ddfb42011d37646f7a2d6815b1365
Scott Lawrence <bytbox@gmail.com>
2011-11-07 09:11:02 -0500

    Non-working attempt to print json representation of /proc to stdout

--- procd.go
+++ procd.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/bytbox/procfs.go/procfs"
+	"json"
+	"log"
+)
 
+func main() {
+	var pfs procfs.ProcFS
+	pfs.Fill()
+	str, err := json.MarshalIndent(pfs, "", "  ")
+	if err != nil {
+		log.Fatal("ERR: ", err)
+	}
+	println(string(str))
 }
